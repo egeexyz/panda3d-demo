cb83843f3ea5f25716757a735e1287308ba6249a
egee-irl <brian@egee.io>
2020-09-19 19:14:50 -0700

    init

---
+++ tutorial/tutorial.py
@@ -0,0 +1,61 @@
+from math import pi, sin, cos
+
+from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+from direct.task import Task
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
+
+
+class Tutorial(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, -2)
+
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+        self.pandaActor.loop("walk")
+
+        # Create the four lerp intervals needed for the panda to
+        # walk back and forth.
+        posInterval1 = self.pandaActor.posInterval(13,
+                                                   Point3(0, -10, 0),
+                                                   startPos=Point3(0, 10, 0))
+        posInterval2 = self.pandaActor.posInterval(13,
+                                                   Point3(0, 10, 0),
+                                                   startPos=Point3(0, -10, 0))
+        hprInterval1 = self.pandaActor.hprInterval(3,
+                                                   Point3(180, 0, 0),
+                                                   startHpr=Point3(0, 0, 0))
+        hprInterval2 = self.pandaActor.hprInterval(3,
+                                                   Point3(0, 0, 0),
+                                                   startHpr=Point3(180, 0, 0))
+
+        self.pandaPace = Sequence(posInterval1, hprInterval1,
+                                  posInterval2, hprInterval2,
+                                  name="pandaPace")
+
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 8
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
+
+def main():
+    game = Tutorial()
+    game.run()
